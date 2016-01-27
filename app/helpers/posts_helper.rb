fadf6700909b4fdbbd78d267c9d0935c5cb7dba2
Danny Baumann <danthemanvt@gmail.com>
2016-01-26 19:57:18 -0800

    Add social shares

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,23 +1,6 @@
 module PostsHelper
 	def post_style(post, current_user)
 		content_tag(:div, id: "post-#{post.id}", class: "post") do
-			content_tag(:div, class: "pull-right") do
-				if user_signed_in? && post.user != current_user
-					unless current_user.favorite_posts.exists?(id: post.id)
-						link_to(raw('<i class="fa fa-star favorite"></i>'), favorite_posts_path(post_id: post.id), 
-						method: :post, remote: true, class: "favorite-button", title: "Favorite!")
-					else	
-						link_to(raw('<i class="fa fa-star unfavorite"></i>'), favorite_post_path(post), 
-						method: :delete, remote: true, class: "favorite-button", title: "Unfavorite.")
-			    end
-			  end
-			end +
-			# content_tag(:div, class: "pull-right") do
-			#   content_tag(:i, "", class: "fa fa-twitter")
-			# end +
-			# content_tag(:div, class: "pull-right") do
-			#   content_tag(:i, "", class: "fa fa-facebook")
-			# end +
 			content_tag(:div, "#{post.body}", class: "post-body") +
 			content_tag(:div, class: "post-user") do
 				if post.user
@@ -25,9 +8,32 @@ module PostsHelper
 				else
 					content_tag(:div, "Anonymous")
 				end
-			end
+			end + 
+			content_tag(:div, class: "post-sharing pull-right") do
+				content_tag(:div, class: "pull-left") do
+					if user_signed_in? && post.user != current_user
+						unless current_user.favorite_posts.exists?(id: post.id)
+							link_to(raw('<i class="fa fa-star favorite"></i>'), favorite_posts_path(post_id: post.id), 
+							method: :post, remote: true, class: "favorite-button", title: "Favorite!")
+						else	
+							link_to(raw('<i class="fa fa-star unfavorite"></i>'), favorite_post_path(post), 
+							method: :delete, remote: true, class: "favorite-button", title: "Unfavorite.")
+				    end
+				  end
+				end +
+				content_tag(:div, class: "pull-left") do
+					link_to(raw('<i class="fa fa-twitter"></i>'), "https://twitter.com/intent/tweet?text=#{post.body} - via @sahdf")
+				end +
+				content_tag(:div, "", class: "fb-like pull-left", 
+					data: { 
+	                href: "#{request.original_url}/posts/#{post.id}", 
+	                action: "like", 
+	                show_faces: "false", 
+	                share: "false", 
+	                layout: "button_count"
+	              } )
+			  end
 		end
-
 		# display = %{
 		# 	<div class="post" id="post-#{post.id}">
 		# 	  <div class="post-body">#{post.body}</div>
