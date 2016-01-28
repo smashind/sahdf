57473b2a65e05bbd20c25d54ef8b4cb82628c1e1
Danny Baumann <danthemanvt@gmail.com>
2016-01-28 04:41:49 -0800

    Add post views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,8 @@
 module PostsHelper
+
 	def post_style(post, current_user)
 		content_tag(:div, id: "post-#{post.id}", class: "post") do
-			content_tag(:div, "#{post.body}", class: "post-body") +
+			content_tag(:a, "#{post.body}", href: post_path(post), class: "post-body") + 
 			content_tag(:div, class: "post-user") do
 				if post.user
 					content_tag(:div, link_to(post.user.name, post.user, class: "user-profile-link"))
@@ -22,7 +23,7 @@ module PostsHelper
 				  end
 				end +
 				content_tag(:div, class: "pull-left") do
-					link_to(raw('<i class="fa fa-twitter"></i>'), "https://twitter.com/intent/tweet?text=#{post.body} - via @sahdfunny")
+					link_to(raw('<i class="fa fa-twitter"></i>'), "https://twitter.com/intent/tweet?text=#{post.body} - via @sahdf")
 				end +
 				content_tag(:div, "", class: "fb-like pull-left", 
 					data: { 
@@ -33,23 +34,8 @@ module PostsHelper
 	                layout: "button_count"
 	              } )
 			end +
-      content_tag(:div, "", class: "clearfix")
+			content_tag(:div, "", class: "clearfix")
 		end
-		# display = %{
-		# 	<div class="post" id="post-#{post.id}">
-		# 	  <div class="post-body">#{post.body}</div>
-		# 	  <div class="post-user">
-		# 	    #{ if post.user ; link_to post.user.name, post.user, class: "user-profile-link" ; else "Anonymous" ; end }
-		# 	  </div>
-		# 	  #{ if user_signed_in? && post.user != current_user ; 
-		# 	    unless current_user.favorite_posts.exists?(id: post.id) ;
-		# 	      link_to raw('<i class="fa fa-star favorite"></i>'), favorite_posts_path(post_id: post.id), method: :post, remote: true, class: "favorite-button", title: "Favorite!" ;
-		# 	    else
-		# 	      link_to raw('<i class="fa fa-star unfavorite"></i>'), favorite_post_path(post), method: :delete, remote: true, class: "favorite-button", title: "Unfavorite." ;
-		# 	    end ;
-		# 	  end }
-		# 	</div>
-		# }
-		# display.html_safe
 	end
+
 end
